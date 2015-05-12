82e14856a661a28a6e87a064c7c85a81533e4a1d
Kyle Nieber <kyle.nieber@gmail.com>
2015-05-12 19:03:37 -0400

    Updated Photo Gallery with PHP to include Original and thumbs

--- photography.php
+++ photography.php
@@ -45,37 +45,16 @@ include('inc/header.php') ?>
         <!-- Gallery Photo's should have max height of 1000px and max width of 700px and put into photoGallery/JPEG -->
         <!-- All Photo's need to be wrapped with an "a" element with an href to the original for full res lightbox. Put originals in photoGallery/JPEG/Originals-->
 
-<!--        <a href="photoGallery/JPEG/Originals/image1.jpg"><img src="photoGallery/JPEG/image1.jpg"></a>-->
-<!--        <a href="photoGallery/JPEG/Originals/image2.jpg"><img src="photoGallery/JPEG/image2.jpg"></a>-->
-<!--        <a href="photoGallery/JPEG/Originals/image3.jpg"><img src="photoGallery/JPEG/image3.jpg"></a>-->
-<!--        <a href="photoGallery/JPEG/Originals/image4.jpg"><img src="photoGallery/JPEG/image4.jpg"></a>-->
-<!--        <a href="photoGallery/JPEG/Originals/image5.jpg"><img src="photoGallery/JPEG/image5.jpg"></a>-->
-<!--        <a href="photoGallery/JPEG/Originals/image6.jpg"><img src="photoGallery/JPEG/image6.jpg"></a>-->
-<!--        <a href="photoGallery/JPEG/Originals/image7.jpg"><img src="photoGallery/JPEG/image7.jpg"></a>-->
-<!--        <a href="photoGallery/JPEG/Originals/image8.jpg"><img src="photoGallery/JPEG/image8.jpg"></a>-->
-<!--        <a href="photoGallery/JPEG/Originals/image9.jpg"><img src="photoGallery/JPEG/image9.jpg"></a>-->
-<!--        <a href="photoGallery/JPEG/Originals/image10.jpg"><img src="photoGallery/JPEG/image10.jpg"></a>-->
-<!--        <a href="photoGallery/JPEG/Originals/image11.jpg"><img src="photoGallery/JPEG/image11.jpg"></a>-->
-<!--        <a href="photoGallery/JPEG/Originals/image12.jpg"><img src="photoGallery/JPEG/image12.jpg"></a>-->
-<!--        <a href="photoGallery/JPEG/Originals/image13.jpg"><img src="photoGallery/JPEG/image13.jpg"></a>-->
-<!--        <a href="photoGallery/JPEG/Originals/image14.jpg"><img src="photoGallery/JPEG/image14.jpg"></a>-->
-<!--        <a href="photoGallery/JPEG/Originals/image15.jpg"><img src="photoGallery/JPEG/image15.jpg"></a>-->
-<!--        <a href="photoGallery/JPEG/Originals/image16.jpg"><img src="photoGallery/JPEG/image16.jpg"></a>-->
-<!--        <a href="photoGallery/JPEG/Originals/image17.jpg"><img src="photoGallery/JPEG/image17.jpg"></a>-->
-<!--        <a href="photoGallery/JPEG/Originals/image18.jpg"><img src="photoGallery/JPEG/image18.jpg"></a>-->
-<!--        <a href="photoGallery/JPEG/Originals/image19.jpg"><img src="photoGallery/JPEG/image19.jpg"></a>-->
-<!--        <a href="photoGallery/JPEG/Originals/image20.jpg"><img src="photoGallery/JPEG/image20.jpg"></a>-->
-<!--        <a href="photoGallery/JPEG/Originals/image21.jpg"><img src="photoGallery/JPEG/image21.jpg"></a>-->
-<!--        <a href="photoGallery/JPEG/Originals/image22.jpg"><img src="photoGallery/JPEG/image22.jpg"></a>-->
-<!--        <a href="photoGallery/JPEG/Originals/image23.jpg"><img src="photoGallery/JPEG/image23.jpg"></a>-->
-<!--        <a href="photoGallery/JPEG/Originals/image24.jpg"><img src="photoGallery/JPEG/image24.jpg"></a>-->
-<!--        <a href="photoGallery/JPEG/Originals/image25.jpg"><img src="photoGallery/JPEG/image25.jpg"></a>-->
 
+          <!-- This php will replace the above HTML once I figure out how to include originals -->
           <?php
 
           $galleryImages = glob("photoGallery/JPEG/*.jpg");
           foreach($galleryImages as $galleryImage) {
-              echo '<a href="' . $galleryImage . '">' . '<img src="' . $galleryImage . '">';
+                $parts = explode("/", $galleryImage);
+                $file = explode(".", $parts[2]);
+                $imagePath = $file[0].".".$file[1];
+            echo '<a href="photoGallery/JPEG/Originals/'.$imagePath.'"><img src="photoGallery/JPEG/'.$imagePath.'"></a>';
           }
 
           ?>
